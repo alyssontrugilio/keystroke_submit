e9fdaeaf20124977708a9b5163f05623571011a1
AlyssonTrugilio <alyson.silvatrugilio121@gmail.com>
2024-07-30 16:03:26 -0300

    feat: implementado increment e decrement no counter pela setas

--- lib/counter_page.dart
+++ lib/counter_page.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 
 import 'app_route.dart';
+
+class IncrementIntent extends Intent {}
+
+class DecrementIntent extends Intent {}
 
 class CounterPage extends StatefulWidget {
   const CounterPage({super.key});
 
   @override
-  State<CounterPage> createState() => _CounterPageState();
+  CounterPageState createState() => CounterPageState();
 }
 
-class _CounterPageState extends State<CounterPage> {
+class CounterPageState extends State<CounterPage> {
   int counter = 0;
 
   void increment() {
-    return setState(() {
+    setState(() {
       counter++;
     });
   }
 
   void decrement() {
-    return setState(() {
+    setState(() {
       counter--;
     });
   }
@@ -34,25 +39,49 @@ class _CounterPageState extends State<CounterPage> {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('$counter'),
-          const SizedBox(height: 100),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              IconButton(
-                onPressed: decrement,
-                icon: const Icon(Icons.arrow_downward_rounded),
-              ),
-              IconButton(
-                onPressed: increment,
-                icon: const Icon(Icons.arrow_upward_rounded),
-              ),
-            ],
+      body: Shortcuts(
+        shortcuts: <LogicalKeySet, Intent>{
+          LogicalKeySet(LogicalKeyboardKey.arrowUp): IncrementIntent(),
+          LogicalKeySet(LogicalKeyboardKey.arrowDown): DecrementIntent(),
+        },
+        child: Actions(
+          actions: <Type, Action<Intent>>{
+            IncrementIntent: CallbackAction<IncrementIntent>(
+              onInvoke: (IncrementIntent intent) => increment(),
+            ),
+            DecrementIntent: CallbackAction<DecrementIntent>(
+              onInvoke: (DecrementIntent intent) => decrement(),
+            ),
+          },
+          child: Focus(
+            autofocus: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '$counter',
+                  style: const TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                const SizedBox(height: 100),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      onPressed: decrement,
+                      icon: const Icon(Icons.arrow_downward_rounded),
+                    ),
+                    IconButton(
+                      onPressed: increment,
+                      icon: const Icon(Icons.arrow_upward_rounded),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ],
+        ),
       ),
     );
   }
